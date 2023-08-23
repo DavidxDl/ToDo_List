8ca9c9be2533c7729f8ad4b5078ef11d6f458a49
DavidxDl <Davidbekamlopez@gmail.com>
2023-08-23 17:27:27 -0400

    testing again this time change the webpack config to support images hope it know render them in dist

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 // Import our custom CSS
-import '../scss/styles.scss'
+import '/src/scss/styles.scss'
+import '/src/scss/style.css'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 import AddForm from './addForm'
