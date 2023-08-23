a0554b4e3dd3c0319207240586a540ff3da3592c
DavidxDl <Davidbekamlopez@gmail.com>
2023-08-23 17:40:52 -0400

    testing again this time change the webpack config to support images hope it know render them in dist

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,15 @@
 // Import our custom CSS
-import '/src/scss/styles.scss'
-import '/src/scss/style.css'
+import '/src/scss/styles.scss';
+import '/src/scss/style.css';
+import '/src/imgs/plus-svgrepo-com (1).svg';
+import 'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto:wght@400;900&display=swap';
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
-import AddForm from './addForm'
-import RenderNotes from './CheckStorage'
+import * as bootstrap from 'bootstrap';
+import AddForm from './addForm';
+import RenderNotes from './CheckStorage';
+import AddNote from './addNote';
+import Note from './TodoNoteClass';
+
 
 const notes = document.querySelector(".notes")
 const addNoteBtn = document.getElementById("addNote")
